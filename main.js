22f7d6cc8d3353cd77fea540bcec800ed2d49cb0
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-03-16 09:00:55 +0500

    commit

--- main.js
+++ main.js
@@ -132,20 +132,6 @@ function rem1(){
 
 
 
-// function factorialize(num) {
-//   if (num < 0) 
-//         return -1;
-//   else if (num == 0) 
-//       return 1;
-//   else {
-//       return (num * factorialize(num - 1));
-//   }
-// }
-// console.log(factorialize(5));
-// factorialize(5);
-
-
-
 
 
 
